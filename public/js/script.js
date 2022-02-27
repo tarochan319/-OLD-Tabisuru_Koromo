82526427d9d5acde428999e675d03bbfcd3d09fc
tarochan319 <tarochan319@gmail.com>
2022-02-27 11:01:55 +0900

    Redesign try

--- public/js/script.js
+++ public/js/script.js
@@ -973,3 +973,17 @@ $(function () {
   });
 });
 
+if (navigator.userAgent.indexOf('iPhone') > 0) {
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.add('iPhone');
+}
+ 
+if (navigator.userAgent.indexOf('iPad') > 0) {
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.add('iPad');
+}
+ 
+if (navigator.userAgent.indexOf('Android') > 0) {
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.add('Android');
+}
